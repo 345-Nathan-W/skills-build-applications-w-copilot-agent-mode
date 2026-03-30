e0647e1c57626ead2f89c354a76f8384c2bd6373
345-Nathan-W <nathan.warwick@345.technology>
2026-03-30 17:09:40 +0000

    Add models for Team, User, Activity, Workout, and Leaderboard

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Users, Teams, Activities, Leaderboard, Workouts models will be added here
+class Team(models.Model):
+	name = models.CharField(max_length=100, unique=True)
+
+	def __str__(self):
+		return self.name
+
+class User(models.Model):
+	name = models.CharField(max_length=100)
+	email = models.EmailField(unique=True)
+	team = models.ForeignKey(Team, related_name='members', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
+class Activity(models.Model):
+	user = models.ForeignKey(User, related_name='activities', on_delete=models.CASCADE)
+	type = models.CharField(max_length=100)
+	duration = models.IntegerField(help_text='Duration in minutes')
+	date = models.DateField()
+
+	def __str__(self):
+		return f"{self.user.name} - {self.type} ({self.date})"
+
+class Workout(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	suggested_for = models.ManyToManyField(Team, related_name='workouts')
+
+	def __str__(self):
+		return self.name
+
+class Leaderboard(models.Model):
+	team = models.OneToOneField(Team, on_delete=models.CASCADE)
+	points = models.IntegerField(default=0)
+
+	def __str__(self):
+		return f"{self.team.name} - {self.points} pts"
